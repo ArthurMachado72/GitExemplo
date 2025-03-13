e0548ab0ed752f256143c7745cb86a8595e18fa1
“ArthurMachado72” <luck10755@gmail.com>
2025-03-13 14:18:37 -0300

    Git Exemplo

--- src/main/java/gitexemplo/GitExemplo/GitExemplo.java
+++ src/main/java/gitexemplo/GitExemplo/GitExemplo.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package gitexemplo.GitExemplo;
 
-/**
- *
- * @author Player14109423498
- */
 public class GitExemplo {
 
     public static void main(String[] args) {
-        System.out.println("GitExemplo!");
+        System.out.println("Hello Word!");
     }
 }
