219d27323336f896850ac824c8e7e0c787ef6c8e
“ArthurMachado72” <luck10755@gmail.com>
2025-03-13 10:53:36 -0300

    Criação do Projeto

---
+++ src/main/java/gitexemplo/GitExemplo/GitExemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gitexemplo.GitExemplo;
+
+/**
+ *
+ * @author Player14109423498
+ */
+public class GitExemplo {
+
+    public static void main(String[] args) {
+        System.out.println("GitExemplo!");
+    }
+}
