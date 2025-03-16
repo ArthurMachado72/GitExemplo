05cac9e50d7c4c5b97e81f68ee7ca510ca3b4369
“ArthurMachado72” <luck10755@gmail.com>
2025-03-16 16:27:06 -0300

    Alteração na main

--- src/main/java/gitexemplo/GitExemplo/GitExemplo.java
+++ src/main/java/gitexemplo/GitExemplo/GitExemplo.java
@@ -4,6 +4,6 @@ package gitexemplo.GitExemplo;
 public class GitExemplo {
 
     public static void main(String[] args) {
-        System.out.println("Nova Feature adicionada!");
+        System.out.println("Alteração na branch main!");
     }
 }
