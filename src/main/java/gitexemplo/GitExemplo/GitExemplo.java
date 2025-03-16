3e1b1c844911da20afca24ec3ecb7c25dd5b66b0
“ArthurMachado72” <luck10755@gmail.com>
2025-03-16 15:06:50 -0300

    Primeiro commit sincronizando repositório local e remoto

--- src/main/java/gitexemplo/GitExemplo/GitExemplo.java
+++ src/main/java/gitexemplo/GitExemplo/GitExemplo.java
@@ -4,6 +4,6 @@ package gitexemplo.GitExemplo;
 public class GitExemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        System.out.println("#Branch");
     }
 }
