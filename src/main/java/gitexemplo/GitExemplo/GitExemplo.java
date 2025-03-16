281399fe9a789b9593ff4e982f53b0e43c98e59b
“ArthurMachado72” <luck10755@gmail.com>
2025-03-16 17:55:19 -0300

    Alteração diferente na main

--- src/main/java/gitexemplo/GitExemplo/GitExemplo.java
+++ src/main/java/gitexemplo/GitExemplo/GitExemplo.java
@@ -5,7 +5,8 @@ public class GitExemplo {
 
     public static void main(String[] args) {
         System.out.println("Alteração na branch conflito-teste!");
-        System.out.println("Nova feature de login!");
+        System.out.println("Alteração diferente na branch main!");
+
 
     }
 }
